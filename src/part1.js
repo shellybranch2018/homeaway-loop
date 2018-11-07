c6860fa02f585359502be9e96e567192a6b6a6df
Shelly Branch <sbranch@homeaway.com>
2018-11-07 14:10:23 -0800

    Part 1 is a work in progress.

---
+++ src/part1.js
@@ -0,0 +1,16 @@
+//Its a start!
+
+function myLoop(){
+
+ for (i=100; i < 201; i++ ){
+  if(i%3 === 0){
+    console.log('home')
+   }
+  if(i%4 === 0){
+    console.log('away')
+   }
+  if (i%3 && i%4 === 0){
+  console.log('HomeAway')
+  }else {console.log(i)}
+   
+ } 
