a16a1c86c99f10aa7aafbff3843febd9af7e5f1a
Shell Branch <sbranch@homeaway.com>
2018-11-08 14:18:08 -0800

    Updated with Batty Beacon script

---
+++ src/part2.js
@@ -0,0 +1,15 @@
+function homeawayLoop(range, multiples, words){
+ for(i = range[0];i < range[1];i++){
+ if(i%multiples[0] === 0){
+    console.log(words[0])
+   }
+  if(i%multiples[1] === 0){
+    console.log(words[1])
+   }
+  if (i%multiples[0] && i%multiples[1] === 0){
+  console.log(words[0]+words[1])
+  }else {console.log(i)}
+ }
+}
+
+homeawayLoop([15, 90],[2, 5],["Batty", "Beacon"])
